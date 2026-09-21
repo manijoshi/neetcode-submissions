c1bb989e019724fc2f4dea7bd45759abcaa5c8ef
manijoshi <65389710+manijoshi@users.noreply.github.com>
2026-09-21 23:35:45 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int CarFleet(int target, int[] position, int[] speed) {
+        Stack<float> fleet = new Stack<float>();
+        var cars = new(int position, int speed)[position.Length];
+    
+        int s = 0;
+        for(int i=0;i<position.Length;i++){
+            cars[i] = (position[i],speed[i]);
+        }
+        Array.Sort(cars,(a,b)=>b.position.CompareTo(a.position));
+    //7,4,1,0
+        for(int i=0;i<cars.Length;i++){
+            float currentTime = (float)(target-cars[i].position)/cars[i].speed;
+            if(fleet.Count==0){
+                fleet.Push(currentTime);
+                s++;
+            }
+            else if(fleet.Count>0 && fleet.Peek()<currentTime){
+                fleet.Push(currentTime);
+                s++;
+            }
+        }
+        return s;
+    }
+}
