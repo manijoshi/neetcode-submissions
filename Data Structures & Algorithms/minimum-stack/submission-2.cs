5ea4e54891f61171ff8bb3f45daf64e736ce4429
manijoshi <65389710+manijoshi@users.noreply.github.com>
2026-09-24 21:43:47 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cs
@@ -0,0 +1,32 @@
+public class MinStack {
+
+    private Stack<int> stack;
+    private Stack<int> minStack;
+
+    public MinStack() {
+        stack = new Stack<int>();
+        minStack = new Stack<int>();
+    }
+    
+    public void Push(int val) {
+        stack.Push(val);
+        if(minStack.Count==0 || minStack.Peek()>=val){
+            minStack.Push(val);
+        }        
+    }
+    
+    public void Pop() {
+        if(stack.Peek()==minStack.Peek()){
+            minStack.Pop();
+        }
+        stack.Pop();
+    }
+    
+    public int Top() {
+        return stack.Peek();
+    }
+    
+    public int GetMin() {
+        return minStack.Peek();
+    }
+}
